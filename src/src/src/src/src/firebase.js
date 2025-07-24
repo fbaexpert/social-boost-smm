0026dbab7b28365d699dff2060ba5997e2984bd0
fbaexpert <ummarfarooq38990@gmail.com>
2025-07-24 10:39:34 +0500

    Update firebase.js

--- src/src/src/src/src/firebase.js
+++ src/src/src/src/src/firebase.js
@@ -1,18 +1,22 @@
+// Import Firebase SDK functions
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your actual Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCwl9P1Vs8Fosdeg7Sz1bT4t5J0Ft-PZEc",
+  authDomain: "social-boost-a31bd.firebaseapp.com",
+  projectId: "social-boost-a31bd",
+  storageBucket: "social-boost-a31bd.appspot.com",
+  messagingSenderId: "8227529400",
+  appId: "1:8227529400:web:1335fdf18893d3292f0a4d"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
